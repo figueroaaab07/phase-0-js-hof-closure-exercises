a35cbaa99e55b87f482ccad822a310eb6bc1f1a0
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-03-25 20:35:45 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-function forEach() {
+const _ = require('lodash');
 
+function forEach(array1, callbackFn) {
+  array1.forEach(callbackFn);
+  return;
 }
 
-function map() {
-
+function map(array1, callbackFn) {
+  return array1.map(callbackFn);;
 }
 
-function filter() {
-
+function filter(array1, callbackFn) {
+  return array1.filter(callbackFn);
 }
 
-function every() {
-
+function every(array1, callbackFn) {
+  return array1.every(callbackFn);
 }
 
-function some() {
-
+function some(array1, callbackFn) {
+  return array1.some(callbackFn);
 }
 
-function majority() {
-
+function majority(array1, callbackFn) {
+  const array2 = filter(array1, callbackFn);
+  return array2.length > (array1.length / 2);
 }
 
-function once() {
-
+function once(callbackFn) {
+  return _.once(callbackFn);
 }
 
-function groupBy() {
-
+function groupBy(array1, callbackFn) {
+  return _.groupBy(array1, callbackFn);
 }
 
-function arrayToObject() {
-
+function arrayToObject(array1, callbackFn) {
+  return _.invert(_.groupBy(array1, callbackFn));
 }
 
 module.exports = { 
